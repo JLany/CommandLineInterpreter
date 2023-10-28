9a37c278319053c47171c5df2b579f0a51270151
DarkenSoda <mahmoudadel200190@yahoo.com>
2023-10-28 19:46:39 +0200

    ls complete

--- Terminal.java
+++ Terminal.java
@@ -2,9 +2,12 @@ import java.nio.file.Path;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Paths;
+import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.nio.file.DirectoryStream;
 import java.nio.file.DirectoryStream.Filter;
 import java.util.Scanner;
@@ -18,7 +21,7 @@ public class Terminal {
     public static void main(String[] args) {
         run = true;
         var in = new Scanner(System.in);
-        
+
         while (run) {
             System.out.print(currentDirFullPath.toString() + ">");
 
@@ -26,9 +29,11 @@ public class Terminal {
 
             boolean isValidInput = parser.parse(input);
 
-            chooseCommandAction();
+            if (isValidInput) {
+                chooseCommandAction();
+            }
         }
-        
+
         in.close();
     }
 
@@ -45,13 +50,16 @@ public class Terminal {
             case "pwd":
                 System.out.printf("%s\n", pwd());
                 break;
+            case "ls":
+                ls(parser.getArgs());
+                break;
             case "exit":
                 run = false;
                 break;
             default:
-                printe(command);;
-        } 
-        
+                printe(command);
+        }
+
     }
 
     // Commands.
@@ -69,18 +77,42 @@ public class Terminal {
         }
 
         inputPath = currentDirFullPath.resolve(inputPath);
-        
+
         try {
             inputPath = inputPath.toRealPath(LinkOption.NOFOLLOW_LINKS);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             printe("the path you specified");
         }
 
         currentDirFullPath = inputPath;
     }
-    
+
     public static void printe(String arg) {
         System.out.printf("%s%s: not found\n", ErrorPrefix, arg);
+    }
+
+    public static void ls(List<String> args) {
+        if (args.size() == 0) {
+            System.out.printf("%-50s %10s\n", "Name", "Last Modified");
+            for (File file : currentDirFullPath.toFile().listFiles()) {
+                System.out.printf("%-50s %10s", file.getName(), new Date(file.lastModified()));
+                System.out.println();
+            }
+        } else if (args.size() == 1) {
+            String directoryPath = currentDirFullPath.toAbsolutePath() + "/" + args.get(0);
+            File directory = new File(directoryPath);
+
+            if (directory.exists() && directory.isDirectory()) {
+                System.out.printf("%-50s %10s\n", "Name", "Last Modified");
+                for (File childFile : directory.listFiles()) {
+                    System.out.printf("%-50s %10s", childFile.getName(), new Date(childFile.lastModified()));
+                    System.out.println();
+                }
+            } else {
+                System.out.println("Invalid directory path or directory doesn't exist.");
+            }
+        } else {
+            System.out.println("Invalid arguments");
+        }
     }
 }
