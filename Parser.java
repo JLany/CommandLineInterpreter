41c663cdd8df3194bab052d138586ad9dd523c7e
Kilany <y.m.elkilany@gmail.com>
2023-10-29 19:54:34 +0200

    Improve Parser.parse method

--- Parser.java
+++ Parser.java
@@ -1,29 +1,83 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.IOError;
+import java.io.IOException;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+enum OutputDirection {
+    Screen,
+    Append,
+    Overwrite
+}
 
 class Parser {
     private String commandName;
+    private OutputDirection outputDirection;
+    private Path outputFilePath;
     private List<String> args;
 
     // This method will divide the input into commandName and args
     // where "input" is the string command entered by the user.
     public boolean parse(String input) {
-        commandName = "";
         args = new ArrayList<>();
-        
-        String[] commandSections = input.trim().split(" ");
+        commandName = "";
+        outputDirection = OutputDirection.Screen;
+        outputFilePath = Paths.get("");
+        boolean isValidInput = true;
 
-        if (commandSections.length < 0) {
-            return true;
+        List<String> commandSections = Arrays.asList(input.trim().split(" "));
+
+        if (commandSections.size() < 1) {
+            return isValidInput;
         }
 
-        commandName = commandSections[0];
+        commandName = commandSections.get(0);
 
-        for (int i = 1; i < commandSections.length; ++i) {
-            args.add(commandSections[i]);
+        int i;
+        for (i = 1; i < commandSections.size(); ++i) {
+            if (commandSections.get(i).equals(">>") 
+                || commandSections.get(i).equals(">")) {
+                break;
+            }
+
+            args.add(commandSections.get(i));
         }
 
-        return true;
+        // Continue parsing the sections of the redirection.
+        if (i < commandSections.size()) {
+            i++;
+            // Either is guarnteed to excute (mutually exclusive).
+            if (commandSections.get(i).equals(">>")) {
+                outputDirection = OutputDirection.Append;
+            } else if (commandSections.get(i).equals(">")) {
+                outputDirection = OutputDirection.Overwrite;
+            }
+
+            try {
+                Path inputPath = Paths.get(commandSections.get(i));
+                Path fileName = inputPath.getFileName();
+
+                outputFilePath = inputPath.getParent()
+                        .toRealPath(LinkOption.NOFOLLOW_LINKS)
+                        .resolve(fileName);
+            } catch (IOException e) {
+                System.out.printf("Could not find a part of the path '%s'\n", commandSections.get(i));
+                isValidInput = false;
+            }
+
+            // If there are more arguments after redirection section.
+            if (i + 1 < commandSections.size()) {
+                System.out.printf("Invalid command syntax.\n");
+                isValidInput = false;
+            }
+        }
+
+        // Each command should further investigate the arguments given to them
+        // to verify that they meet the requirements of the command to run.
+        return isValidInput;
     }
 
     public String getCommandName() {
@@ -33,5 +87,13 @@ class Parser {
     public List<String> getArgs() {
         return args;
     }
-}
 
+    public void writeOutput(String format, Object... args) {
+        if (outputDirection == OutputDirection.Screen) {
+            System.out.printf(format, args);
+            return;
+        }
+
+        
+    }
+}
--- Terminal.java
+++ Terminal.java
@@ -43,6 +43,7 @@ public class Terminal {
         if (command.isBlank()) {
             return;
         }
+        
         switch (command) {
             case "cd":
                 cd(parser.getArgs());
